97c695b36c4fa0b30534dba89bf7922898686e71
Max Nicolai <maxvnicolai@gmail.com>
2020-10-22 15:21:22 -0700

    Post route  to create new workout added

--- routes/api.js
+++ routes/api.js
@@ -7,10 +7,12 @@ const exercise = require('../models')
 
 //Define required routes
 
-//Get workouts sorted by date: get /api/workouts
+//Get workouts sorted by date
 router.get('/api/workouts', (req, res) => {
   db.Workout.find({})
+    //Sorts day in ascending order, meaning the most recent workout is last
     .sort({ day: 1})
+    //Return sorted workouts, or throw error 
     .then(result => {
       res.json(result)
       console.log(result)
@@ -19,8 +21,20 @@ router.get('/api/workouts', (req, res) => {
       res.status(400).json(err)
     });
 })
+
 //Add exercise: put api/workouts
+
 //Add workout: post api/workouts
+router.post('/api/workouts', ({ body }, res) => {
+  db.Workout.create(body)
+  .then((result) => {
+    res.json(result)
+    console.log(result)
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  });
+})
 
 //Get workouts in specified range: get /api/workouts/range
 
