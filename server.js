b4b64f744b493c27592b91e6b9a9782a55cbfe3a
Max Nicolai <maxvnicolai@gmail.com>
2020-10-23 16:06:24 -0700

    added params to mongoose.connect to access db in deployed environment

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 //Create connection to database
-mongoose.connect('mongodb://localhost/workout', {useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 //Create an express app
 const app = express();
