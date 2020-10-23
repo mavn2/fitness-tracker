23c4c4343446b7414779b32a4b69946c5fab25b2
Max Nicolai <maxvnicolai@gmail.com>
2020-10-23 16:34:42 -0700

    deploying

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 //Export model through index so that server can  require the entire file, simplifying
 //The process of adding any additional models
 const Workout = require('./Workout')
+//Exports model(s) as values in an object
 module.exports = {Workout: Workout}
