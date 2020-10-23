65156ba4792078dcd9c69e118eac93cded52f535
Max Nicolai <maxvnicolai@gmail.com>
2020-10-23 15:26:49 -0700

    Added /range route, workout dashboard now displays. Removed console.log s

--- routes/api.js
+++ routes/api.js
@@ -12,10 +12,11 @@ router.get('/api/workouts', (req, res) => {
   db.Workout.find({})
     //Sorts day in ascending order, meaning the most recent workout is last
     .sort({ day: 1})
-    //Return sorted workouts, or throw error 
+    //Return sorted workouts
     .then(result => {
       res.json(result)
     })
+    //Throw error if request fails
     .catch(err => {
       res.status(400).json(err)
     });
@@ -23,31 +24,47 @@ router.get('/api/workouts', (req, res) => {
 
 //Add exercise: put api/workouts
 router.put('/api/workouts/:id', (req, res) => {
-  console.log('id ' + req.params.id)
-  console.log('body ' + req.body)
+  //Find workout to update based on id passed in url parameters, add exercise in request body
+  //Setting new to true means the updated workout is returned
   db.Workout.findOneAndUpdate({ '_id': req.params.id }, { exercises: req.body }, { new: true})
+    //Return workout with added exercise
     .then(result => {
       res.json(result);
-      console.log('result' + result);
     })
+    //Throw error if request fails
     .catch(err => {
       res.status(400).json(err)
     });
 })
 //Add workout: post api/workouts
 router.post('/api/workouts', ({ body }, res) => {
+  //Create a workout object
   db.Workout.create(body)
+  //Returned created workout
   .then((result) => {
     res.json(result)
-    console.log(result)
   })
+  //Throw error if request fails
   .catch(err => {
     res.status(400).json(err)
   });
 })
 
 //Get workouts in specified range: get /api/workouts/range
-
+router.get('/api/workouts/range', (req, res) => {
+  //Fetch all workouts from database, as requested in stats.js
+  db.Workout.find({})
+  //Sort workouts by date
+    .sort({ day: 1})
+  //Return list of workouts
+  .then(result => {
+    res.json(result)
+  })
+  //Throw error if request fails
+  .catch(err => {
+    res.status(400).json(err)
+  });
+})
 
 //Export router w/ added methods for use by app
 module.exports = router;
